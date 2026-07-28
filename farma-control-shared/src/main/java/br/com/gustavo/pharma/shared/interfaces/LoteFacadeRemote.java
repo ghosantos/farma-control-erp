42cb513bfc7912dd0ab2233526411af581fcce2d
ghosantos <gustavo.santos7611@gmail.com>
2026-07-28 13:50:00 -0300

    feat(shared): define metodos da interface LoteFacadeRemote

--- farma-control-shared/src/main/java/br/com/gustavo/pharma/shared/interfaces/LoteFacadeRemote.java
+++ farma-control-shared/src/main/java/br/com/gustavo/pharma/shared/interfaces/LoteFacadeRemote.java
@@ -1,7 +1,17 @@
 package br.com.gustavo.pharma.shared.interfaces;
 
+import br.com.gustavo.pharma.shared.dto.*;
 import jakarta.ejb.Remote;
+
+import java.util.List;
 
 @Remote
 public interface LoteFacadeRemote {
+    LoteDTO incluirLote(LoteCreateDTO dto);
+    LoteDTO buscarPorId(Long id);
+    List<LoteDTO> buscarTodos();
+    List<LoteDTO> buscarPorProdutoId(Long produtoId);
+    LoteDTO atualizarLote(Long id, LoteUpdateDTO loteUpdateDTO);
+    void deletarLote(Long id);
+    LoteDTO darBaixaLote(Long loteId, Integer quantidade);
 }
