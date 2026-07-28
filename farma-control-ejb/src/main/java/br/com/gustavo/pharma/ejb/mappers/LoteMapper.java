dda1376140a330fb64add98b777c1b5e3200bffc
ghosantos <gustavo.santos7611@gmail.com>
2026-07-28 15:05:46 -0300

    refactor(ejb): atualiza LoteMapper para utilizar id e LoteCreateDTO
    Mapeia o id da entidade no toDTO e altera o parametro do metodo toEntity de LoteDTO para LoteCreateDTO

--- farma-control-ejb/src/main/java/br/com/gustavo/pharma/ejb/mappers/LoteMapper.java
+++ farma-control-ejb/src/main/java/br/com/gustavo/pharma/ejb/mappers/LoteMapper.java
@@ -1,17 +1,28 @@
 package br.com.gustavo.pharma.ejb.mappers;
 
 import br.com.gustavo.pharma.ejb.entities.Lote;
+import br.com.gustavo.pharma.shared.dto.LoteCreateDTO;
 import br.com.gustavo.pharma.shared.dto.LoteDTO;
+import br.com.gustavo.pharma.shared.dto.ProdutoDTO;
 
 public class LoteMapper {
 
     public static LoteDTO toDTO(Lote lote){
-        return new LoteDTO(lote.getNumeroLote(), lote.getQuantidadeAtual(),
-                lote.getDataFabricacao(), lote.getDataValidade());
+        return new LoteDTO(
+                lote.getId(),
+                lote.getNumeroLote(),
+                lote.getQuantidadeAtual(),
+                lote.getDataFabricacao(),
+                lote.getDataValidade()
+        );
     }
 
-    public static Lote toEntity(LoteDTO loteDTO){
-        return new Lote(loteDTO.getNumeroLote(), loteDTO.getQuantidadeAtual(),
-                loteDTO.getDataFabricacao(), loteDTO.getDataValidade());
+    public static Lote toEntity(LoteCreateDTO loteDTO){
+        return new Lote(
+                loteDTO.getNumeroLote(),
+                loteDTO.getQuantidadeAtual(),
+                loteDTO.getDataFabricacao(),
+                loteDTO.getDataValidade()
+        );
     }
 }
